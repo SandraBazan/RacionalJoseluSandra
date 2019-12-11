25981d7042c158109542dbc7c8a7941c19a4b440
sandra <sandra.bj0308@gmail.com>
2019-12-11 14:34:58 +0100

    Operaciones finales, comprobación de los métodos y formateado del código.

--- src/main/java/tarea5git/AppRacional.java
+++ src/main/java/tarea5git/AppRacional.java
@@ -10,19 +10,36 @@ package tarea5git;
  * @author joselu
  */
 public class AppRacional {
+
     // Creación del método main
     public static void main(String[] args) {
         // Creación de los dos racionales con valores
-        Racional z = new Racional(4,5);
-        Racional k = new Racional (3,4);
-        
+        Racional z = new Racional(4, 5);
+        Racional k = new Racional(3, 4);
+
         // Creación del número racional aleatorio
         Racional aleatorio = Racional.aleatorio();
-        
+
         // Imprimimos por pantalla los tres racionales
         System.out.println(z);
         System.out.println(k);
         System.out.println(aleatorio);
+
+        // Operaciones
+        // Igualdad
+        System.out.println("¿El racional z y el racional k son iguales? " + Racional.igualdad(z, k));
+
+        // Suma
+        z.suma(k);
+        System.out.println("La suma de z+k es : " + z.getA() + "/" + z.getB());
+
+        // División
+        System.out.println("La división de z entre k es: " + Racional.division(z, k));
+
+        // Producto
+        z.producto(k);
+        System.out.println("El producto de z por k es: " + z.getA() + "/" + z.getB());
+
     }
-   
+
 }
