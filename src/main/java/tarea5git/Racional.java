fbfd72f8482269317da105c4ad125121d720a4d4
sandra <sandra.bj0308@gmail.com>
2019-12-11 13:16:23 +0100

    Declaración de atributos y creación del constructor por defecto

--- src/main/java/tarea5git/Racional.java
+++ src/main/java/tarea5git/Racional.java
@@ -10,5 +10,15 @@ package tarea5git;
  * @author sandra
  */
 public class Racional {
+    // Creación de los atributos
+    // Creamos el numerador
+    private int a;
+    // Crramos el denominador
+    private int b;
     
+    // Constructor por defecto que inicializa a ambos a 1
+    public Racional(){
+        this.a = 1;
+        this.b = 1;
+    }
 }
