31a92330a5d64c154ff01e5abe7d2cd1a070cf04
sandra <sandra.bj0308@gmail.com>
2019-12-11 13:10:22 +0100

    Creación del proyecto

---
+++ src/main/java/tarea5git/Racional.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea5git;
+
+/**
+ *
+ * @author sandra
+ */
+public class Racional {
+    
+}
