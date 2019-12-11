88be11d6ba274e691fcfb102e0f426431cb5a57f
Joselu Aguilar <joselubiker@gmail.com>
2019-12-11 14:21:04 +0100

    Creación de los números racionales e imprimirlos por pantalla

--- src/main/java/tarea5git/AppRacional.java
+++ src/main/java/tarea5git/AppRacional.java
@@ -10,8 +10,19 @@ package tarea5git;
  * @author joselu
  */
 public class AppRacional {
+    // Creación del método main
     public static void main(String[] args) {
+        // Creación de los dos racionales con valores
+        Racional z = new Racional(4,5);
+        Racional k = new Racional (3,4);
         
+        // Creación del número racional aleatorio
+        Racional aleatorio = Racional.aleatorio();
+        
+        // Imprimimos por pantalla los tres racionales
+        System.out.println(z);
+        System.out.println(k);
+        System.out.println(aleatorio);
     }
    
 }
